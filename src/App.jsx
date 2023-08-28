0dfaedc6f6f22366b5b3253b0aba08b78a39fbc9
AdityaRaj0001 <meadityaraj0001@gmail.com>
2023-08-28 12:18:48 +0530

    all good just one issue is the width of each container

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+import data from './card-data'
+import Card from './components/card'
+const App = () => {
+  return (
+    <div className="bg-neutral-800">
+      <div className="flex h-48 items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll down
+        </span>
+      </div>
+      <HorizontalScrollCarousel />
+      <div className="flex h-48 items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll up
+        </span>
+      </div>
+    </div>
+  );
+};
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex h-auto justify-between gap-4 ">
+          {data.map((card_data,key) => {
+            return <Card  title={card_data.title} desc={card_data.desc} Icon={card_data.icon} key={key} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
+
