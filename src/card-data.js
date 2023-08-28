4ff87d2086223e99d2ac605a54abef4708e32a81
AdityaRaj0001 <meadityaraj0001@gmail.com>
2023-08-28 18:23:06 +0530

    fixed height issue with the card

--- src/card-data.js
+++ src/card-data.js
@@ -1,4 +1,4 @@
-import { FiCreditCard, FiMail,} from "react-icons/fi";
+import { FiCreditCard, FiMail, FiUsers,FiArchive,FiDownload,FiGitlab,FiAnchor} from "react-icons/fi";
 
 const  cards = [
     {
@@ -16,111 +16,76 @@ const  cards = [
     {
         title:"Do Not Disturb (DnD)",
         desc:"Enjoy uninterrupted focus by setting your status to Do Not Disturb mode. Avoid interruptions and reply when you're ready.",
-        icon:FiMail,
+        icon:FiAnchor,
         id:"3",
     },
     {
         title:"Password-Free Lock Screen",
         desc:"We prioritize security. Replace traditional passwords with a unique random pattern for your lock screen, making unauthorized access nearly impossible.",
-        icon:FiMail,
+        icon:FiGitlab,
         id:"4",
     },
     {
         title:"App Icon Variation",
         desc:"Embrace variety with regularly changing app icons. Keep things fresh and discreet on your device's home screen.",
-        icon:FiMail,
+        icon:FiDownload,
         id:"5",
     },
     {
         title:"Chat Auto Hide",
         desc:"Take security up a notch. Chats remain hidden on your home screen until you provide a password, offering an additional layer of protection to your conversations.",
-        icon:FiMail,
+        icon:FiUsers,
         id:"6",
     },
     {
         title:"Emergency Data Wipe",
         desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
+        icon:FiArchive,
         id:"7",
+    },
+    {
+      title:"Username Change Timer",
+      desc:"Your privacy matters. Our app automatically changes your username at set intervals, adding an extra layer of protection to your online identity.",
+      icon:FiCreditCard,
+      id:"8",
+    },
+    {
+        title:"Ghost Mode",
+        desc:"Stay completely hidden while using the app. No one will know when you're online, ensuring your privacy and allowing you to engage on your terms.",
+        icon:FiMail,
+        id:"9",
+    },
+    {
+        title:"Do Not Disturb (DnD)",
+        desc:"Enjoy uninterrupted focus by setting your status to Do Not Disturb mode. Avoid interruptions and reply when you're ready.",
+        icon:FiAnchor,
+        id:"10",
+    },
+    {
+        title:"Password-Free Lock Screen",
+        desc:"We prioritize security. Replace traditional passwords with a unique random pattern for your lock screen, making unauthorized access nearly impossible.",
+        icon:FiGitlab,
+        id:"11",
+    },
+    {
+        title:"App Icon Variation",
+        desc:"Embrace variety with regularly changing app icons. Keep things fresh and discreet on your device's home screen.",
+        icon:FiDownload,
+        id:"12",
+    },
+    {
+        title:"Chat Auto Hide",
+        desc:"Take security up a notch. Chats remain hidden on your home screen until you provide a password, offering an additional layer of protection to your conversations.",
+        icon:FiUsers,
+        id:"13",
     },
     {
         title:"Emergency Data Wipe",
         desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
+        icon:FiArchive,
+        id:"14",
     },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
-    {
-        title:"Emergency Data Wipe",
-        desc:"Take control of your contacts. Approve friend requests before they're accepted, ensuring your social circle aligns with your preferences.",
-        icon:FiMail,
-        id:"7",
-    },
+
   ];
 
   export default cards
